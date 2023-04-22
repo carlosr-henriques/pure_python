9cf7141e23d85c72ace720e492889edf8c42068e
Carlos Roberto <48866075+carlosr-henriques@users.noreply.github.com>
2023-04-21 22:19:43 -0300

    Update user_guess_the_number.py

--- guess_number/user_guess_the_number.py
+++ guess_number/user_guess_the_number.py
@@ -1,14 +1,14 @@
 from random import randint
 
-def user_guess_number(final_limit_value: int, guess: int):
+def user_guess_number(final_limit_value: int, guess:int) -> None:
     random_number = randint(1, final_limit_value)
-    
-    while random_number != guess:
-        print(f"Try to hit a number between 1 and {guess}")
-        if random_number < guess:
+    user_number = int(input("Enter with your random number: "))
+    while random_number != user_number:
+        print(f"Try to hit a number between 1 and {final_limit_value}")
+        if random_number < user_number:
             print("Sorry, you're wrong. Try again too low!")
             break
-        elif random_number > guess:
+        elif random_number > user_number:
             print("Sorry, you're wrong. Try again too high!")
             break
         else:
@@ -16,6 +16,6 @@ def user_guess_number(final_limit_value: int, guess: int):
             break
 
 if __name__ == '__main__':
-    final_limit_value = int(input("Enter the final number in the interval between values."))
+    final_limit_value = int(input("Enter the final number in the interval between values: "))
     guess = randint(1, final_limit_value)
     user_guess_number(final_limit_value, guess)
