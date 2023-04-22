9eff9a9e57094f9cbf709f4d5a3dcee1d8967d7d
Carlos Roberto <48866075+carlosr-henriques@users.noreply.github.com>
2023-04-21 21:24:05 -0300

    Add files via upload

---
+++ guess_number/user_guess_the_number.py
@@ -0,0 +1,21 @@
+from random import randint
+
+def user_guess_number(final_limit_value: int, guess: int):
+    random_number = randint(1, final_limit_value)
+    
+    while random_number != guess:
+        print(f"Try to hit a number between 1 and {guess}")
+        if random_number < guess:
+            print("Sorry, you're wrong. Try again too low!")
+            break
+        elif random_number > guess:
+            print("Sorry, you're wrong. Try again too high!")
+            break
+        else:
+            print(f"Congrats! You're right.")
+            break
+
+if __name__ == '__main__':
+    final_limit_value = int(input("Enter the final number in the interval between values."))
+    guess = randint(1, final_limit_value)
+    user_guess_number(final_limit_value, guess)
